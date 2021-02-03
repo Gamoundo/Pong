fe22022d3155781cf156bcc13a22012dd46b767e
Gamoundo <akilgg@gmail.com>
2021-02-03 16:57:08 -0500

    svg pong

---
+++ src/Pong.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Pong() {
+    return(
+        <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="800" height="800" color='black'>
+   
+</svg> 
+        </div>
+    )
+}
+
+export default Pong;
