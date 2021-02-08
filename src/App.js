bf4c0e66aea76620ffc6fb8ea0f3a6b3246bd3fd
Gamoundo <akilgg@gmail.com>
2021-02-07 21:57:23 -0500

    added canvas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import Canvas from './Canvas';
+
 
 import { ReactComponent as Logo } from './logo.svg';
 
@@ -8,7 +10,7 @@ function App() {
     <div className="App">
       <h1> Pong</h1>
       <Logo />
-       
+       <Canvas />
     </div>
   );
 }
