90c2659826757204be49fbe5e1090c6a5cef2f03
Gamoundo <akilgg@gmail.com>
2021-04-19 18:07:37 -0400

    added some vars for the x coordinates

--- src/Canvas.js
+++ src/Canvas.js
@@ -4,9 +4,12 @@ const Canvas = props => {
   
   const canvasRef = useRef(null)
 
+  let ballx= 400
+  let paddleY=200
   const drawLeftPaddle = rect => {
+    paddleY +=50
     rect.fillStyle = 'red'
-    rect.fillRect(0,200, 20, 100)
+    rect.fillRect(0,paddleY, 20, 100)
   }
 
   const drawRightPaddle = rect => {
@@ -15,22 +18,23 @@ const Canvas = props => {
   }
 
   const drawBall = ctx => {
+    ballx +=200
     ctx.fillStyle = 'white'
     ctx.beginPath()
-    ctx.arc(400, 400, 10, 0, 2*Math.PI)
+    ctx.arc(ballx, 400, 10, 0, 2*Math.PI)
     ctx.fill()
   }
   
   useEffect(() => {
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
-    //Our first draw
+    
     context.fillStyle = '#000000'
     context.fillRect(0, 0, 800, 800)
     drawLeftPaddle(context)
     drawRightPaddle(context)
     drawBall(context)
-  }, [drawLeftPaddle])
+  }, [])
   
   return <canvas ref={canvasRef} width={800} height={800} {...props}/>
 }
